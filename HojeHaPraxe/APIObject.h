31bfaa42dcc57baaeaee694e10937c58bf11babd
João Bernardo <jb.amaro@hotmail.com>
2014-06-07 16:16:02 +0100

    API Object implemented the way it should be

---
+++ HojeHaPraxe/APIObject.h
@@ -0,0 +1,17 @@
+//
+//  APIObject.h
+//  HojeHaPraxe
+//
+//  Created by João Bernardo on 07/06/14.
+//  Copyright (c) 2014 jeKnowledge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIObject : NSObject
+
+@property (strong, nonatomic) NSString *hapraxe;
+@property (strong, nonatomic) NSString *reason;
+@property (strong, nonatomic) NSString *notification;
+
+@end
---
+++ HojeHaPraxe/APIObject.m
@@ -0,0 +1,15 @@
+//
+//  APIObject.m
+//  HojeHaPraxe
+//
+//  Created by João Bernardo on 07/06/14.
+//  Copyright (c) 2014 jeKnowledge. All rights reserved.
+//
+
+#import "APIObject.h"
+
+@implementation APIObject
+
+//Code 
+
+@end
