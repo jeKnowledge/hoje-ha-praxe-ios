c6350ecbecee75eced8d23b6520d3ab606644b1e
David Gomes <davidrafagomes@gmail.com>
2014-06-07 01:04:14 +0100

    Initial Commit

---
+++ HojeHaPraxe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HojeHaPraxe
+//
+//  Created by David Gomes on 07/06/14.
+//  Copyright (c) 2014 jeKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HojeHaPraxe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HojeHaPraxe
+//
+//  Created by David Gomes on 07/06/14.
+//  Copyright (c) 2014 jeKnowledge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
