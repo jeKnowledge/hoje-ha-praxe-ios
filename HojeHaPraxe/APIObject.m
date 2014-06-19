2fda35107b9be3b75c812244b5a49883cf52c776
João Bernardo <jb.amaro@hotmail.com>
2014-06-19 13:00:48 +0100

    Small improvement in APIObject.m

--- HojeHaPraxe/APIObject.m
+++ HojeHaPraxe/APIObject.m
@@ -15,14 +15,13 @@
 {
     NSData *praxeData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://jbernardo.com/result.html"]];
     
-    NSError *error;
-    
     if (praxeData == nil) // Error: server not available
     {
         return nil;
     }
     else
     {
+        NSError *error;
         NSMutableDictionary *praxeDataOrganized = [NSJSONSerialization JSONObjectWithData:praxeData options: NSJSONReadingMutableContainers error:&error];
         if(error) // Error: JSON parsing failed
         {
